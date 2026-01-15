ebbde351c183e5c93fa741596808b7222c0c0c30
llanerosolitariio-commits <llanerosolitariio@gmail.com>
2026-01-14 21:15:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
+const cacheName = 'ultranav-v2';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(['./index.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
